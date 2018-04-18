fc2f50a31dcd6acc0b485f08e0d178b8825877b7
Simon Siverland <Simon@Simons-MacBook-Pro-2.local>
2018-04-18 10:22:08 +0200

    First vomit

---
+++ relay.m
@@ -0,0 +1,29 @@
+%% First case
+s1 = 0;
+s2 = 0;
+R = 0;
+D = 0;
+p10 = 0.9;
+p13 = 0.25;
+p20 = 0.9;
+p23 = 0.25;
+p03 = 0.9;
+lambda1 = 0;
+lambda2 = 0;
+lambda0 = 0;
+slots = 100000;
+
+%% Second case
+s1;
+s2;
+R;
+D;
+p10 = 0.9;
+p13 = 0.4;
+p20 = 0.9;
+p23 = 0.4;
+p03 = 0.95;
+lambda1;
+lambda2;
+lambda0;
+slots = 100000;
