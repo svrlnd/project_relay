1ae2b6c2d6c4e67dc2f61b13a47ed2fbee195d07
Gustav <gsedvallson@gmail.com>
2018-04-25 16:01:25 +0200

    Gjort en tom for-loop och skrivit kommentarer om hur mycket livet kan suga ibland

--- relay.m
+++ relay.m
@@ -8,10 +8,30 @@ p13 = 0.25;
 p20 = 0.9;
 p23 = 0.25;
 p03 = 0.9;
-lambda1 = linspace(0,1,11); %Gör så vi kan plotta grejer mot olika lambdavärden
+q1 = 0.3;
+q2 = 0.3;
+q0 = 0.45;
+lambda1 = linspace(0,1,11); % Gör så vi kan plotta grejer mot olika lambdavärden
 lambda2 = linspace(0,1,11); %
-lambda0 = 0;
-slots = 100000;
+lambda0 = lambda1 * p10 + lambda2' * p20; % Här ska vi ha en linj.kombination av 
+                                          % lambda1 och lambda2. (MEN vafan
+                                          % det här är ju bara då vi har 0
+                                          % kö) kuk ollon
+numberOfSlots = 10000;
+timeslots = zeros(1,10000);
+
+
+
+for i = 1:numberOfSlots 
+    %gör nåt som fyller på timeslotsen när dom blir upptagna
+    %if timeslot "i" är fylld kan vi inte transmitta nåt (sätt nån spärr genom if-sats)
+    
+    
+end
+
+    
+
+
 
 %% Second case
 s1;
@@ -23,7 +43,14 @@ p13 = 0.4;
 p20 = 0.9;
 p23 = 0.4;
 p03 = 0.95;
+q1 = 0.3;
+q2 = 0.3;
+q0 = 0.45;
 lambda1 = linspace(0,1,11); %Gör så vi kan plotta grejer mot olika lambdavärden
 lambda2 = linspace(0,1,11); %
 lambda0;
-slots = 100000;
+timeslots = zeros(1,10000);
+
+
+
+
